11aaea2460840ef7c8bea3b3d7bff755ec27a3f1
Ole Schley <ole.schley@oldendorff.com>
2020-04-10 21:39:44 +0800

    initial commit

---
+++ wikisearch/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  pages = [];
+
+  constructor(private wikipedia: WikipediaService) { }
+
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search
+    });
+  }
+}
+
